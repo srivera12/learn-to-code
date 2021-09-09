f22b495a7ec4d225ce77324439dbd1ee3c4eafea
Sarah Rivera <sarahkrivera@gmail.com>
2021-09-09 11:02:15 -0400

    arrays app: insult generator

---
+++ javascript-beginner-bootcamp/randomInsult.js
@@ -0,0 +1,9 @@
+const randomBodyParts = ["head", "butt", "legs", "face"];
+const randomAdjectives = ["stupid", "ugly", "poopy", "farty"];
+const randomWords = ["director", "wood", "soup", "outcome", "negotiation"];
+
+const randomBodyPart = randomBodyParts[Math.floor(Math.random() * 4)];
+const randomAdjective = randomAdjectives[Math.floor(Math.random() * 4)];
+const randomWord = randomWords[Math.floor(Math.random() * 5)];
+
+console.log("Your " + randomBodyPart + " is like a " + randomAdjective + " " + randomWord + "!!!!");
