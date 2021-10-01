4a64c3a87f0c68f417583e72eb9ea8124baf5a6b
Sarah Rivera <sarahkrivera@gmail.com>
2021-10-01 12:22:18 -0400

    make 20 websites course content

---
+++ make-20-websites/basic-site-redux.js
@@ -0,0 +1,43 @@
+const banner = document.getElementById('banner');
+
+function changeText1() {
+  document.getElementById('title-change').innerHTML = 'Weathervane';
+  document.getElementById('paragraph-change').innerHTML =
+    'Dogs nails at est. Ewes fox, hay hook hay manure, John Deere radish barn, a hay loft house at pony. Grapes nest pitch fork an plows maple syrup yearlings, quilt squeak doggies. Onion organic oranges and purr ducks canning owls at a squeal. Outhouse.';
+  banner.style.backgroundColor = '#c1dce0';
+}
+
+function changeText2() {
+  document.getElementById('title-change').innerHTML = 'Plows';
+  document.getElementById('paragraph-change').innerHTML =
+    'Forage Harvester, bean and Silage dump, cultivator brussel sprouts harrows, celery dread with kale augers harrows. Shovels at rakes plows. In eggplant, quonset is grain bins, grain trucks quonset pole shed, with fences gates zucchini carrots scrap metal.';
+  banner.style.backgroundColor = '#91d956';
+}
+
+function changeText3() {
+  document.getElementById('title-change').innerHTML = 'Barn';
+  document.getElementById('paragraph-change').innerHTML =
+    'A squeal. Peacocks baa ostriches owls. Utters are weathervane foal est. Gobble feed, jelly peppers at plow basil swather, cat weathervane grain trucks, hoot pony robins peacocks an kale. Peacocks baa ostriches owls. Sage mower goat, raccoon.';
+  banner.style.backgroundColor = '#aa4008';
+}
+
+function changeText4() {
+  document.getElementById('title-change').innerHTML = 'Trucks';
+  document.getElementById('paragraph-change').innerHTML =
+    'Rhubarb outhouse a, apples berries corn. Feed in a woof, a farmers market. Prairie dogs raccoons robins rats. Lamb pig rooster sheep. Petting zoo at carrots alligators quack. Mouse soybeans sweet corn hogs llamas or oink oink wind. Quack hammers eggplant is utters nails garden.';
+  banner.style.backgroundColor = '#2b601e';
+}
+
+function changeText5() {
+  document.getElementById('title-change').innerHTML = 'Gobble';
+  document.getElementById('paragraph-change').innerHTML =
+    'A, apples berries corn. Outhouse at nails mower. Gate wind, moonshine horses meow irrigation , with feed troughs cheep, or cabbage with pumpkin trees chicken. Oranges cucumbers rhubarb gourds watermelon. John Deere bees, parsley sweet corn at, porky pig shovels.';
+  banner.style.backgroundColor = '#796829';
+}
+
+function changeText6() {
+  document.getElementById('title-change').innerHTML = 'Irrigation';
+  document.getElementById('paragraph-change').innerHTML =
+    'At, farmers market and cultivator ostrich. Prairie dogs raccoons robins rats. In the straw rain barrels. Bulls at rose garden cucumbers mice sunflower wheat in pig. Grapes nest pitch fork an plows maple syrup yearlings, quill. Donkey duck.';
+  banner.style.backgroundColor = '#46a908';
+}
