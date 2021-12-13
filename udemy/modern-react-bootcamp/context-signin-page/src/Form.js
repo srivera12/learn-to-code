b895e40515b5eb66b3f6d695ae5979dcffd450e1
Sarah Rivera <sarahkrivera@Sarahs-MBP.fios-router.home>
2021-12-13 10:48:21 -0500

    refactoring context mini-app to use hooks and gitignore edit

--- udemy/modern-react-bootcamp/context-signin-page/src/Form.js
+++ udemy/modern-react-bootcamp/context-signin-page/src/Form.js
@@ -13,7 +13,7 @@ import {
 } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import { LockOutlined } from '@material-ui/icons';
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { LanguageContext } from './contexts/LanguageContext';
 import styles from './styles/FormStyles';
 
@@ -38,42 +38,39 @@ const text = {
   },
 };
 
-class Form extends Component {
-  static contextType = LanguageContext;
-  render() {
-    const { classes } = this.props;
-    const { language, changeLanguage } = this.context;
-    const { signIn, email, password, remember } = text[language];
-    return (
-      <main className={classes.main}>
-        <Paper className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlined />
-          </Avatar>
-          <Typography variant="h5">{signIn}</Typography>
-          <Select value={language} onChange={changeLanguage}>
-            <MenuItem value="english">English</MenuItem>
-            <MenuItem value="spanish">Spanish</MenuItem>
-            <MenuItem value="french">French</MenuItem>
-          </Select>
-          <form className={classes.form}>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="email">{email}</InputLabel>
-              <Input id="email" name="email" autoFocus></Input>
-            </FormControl>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="password">{password}</InputLabel>
-              <Input id="password" name="password"></Input>
-            </FormControl>
-            <FormControlLabel control={<Checkbox color="primary" />} label={remember} />
-            <Button variant="contained" type="submit" fullWidth color="primary" className={classes.submit}>
-              {signIn}
-            </Button>
-          </form>
-        </Paper>
-      </main>
-    );
-  }
+function Form(props) {
+  const { language, changeLanguage } = useContext(LanguageContext);
+  const { classes } = props;
+  const { signIn, email, password, remember } = text[language];
+  return (
+    <main className={classes.main}>
+      <Paper className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlined />
+        </Avatar>
+        <Typography variant="h5">{signIn}</Typography>
+        <Select value={language} onChange={changeLanguage}>
+          <MenuItem value="english">English</MenuItem>
+          <MenuItem value="spanish">Spanish</MenuItem>
+          <MenuItem value="french">French</MenuItem>
+        </Select>
+        <form className={classes.form}>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="email">{email}</InputLabel>
+            <Input id="email" name="email" autoFocus></Input>
+          </FormControl>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="password">{password}</InputLabel>
+            <Input id="password" name="password"></Input>
+          </FormControl>
+          <FormControlLabel control={<Checkbox color="primary" />} label={remember} />
+          <Button variant="contained" type="submit" fullWidth color="primary" className={classes.submit}>
+            {signIn}
+          </Button>
+        </form>
+      </Paper>
+    </main>
+  );
 }
 
 export default withStyles(styles)(Form);
--- udemy/modern-react-bootcamp/context-signin-page/src/NavBar.js
+++ udemy/modern-react-bootcamp/context-signin-page/src/NavBar.js
@@ -1,8 +1,8 @@
 import { AppBar, IconButton, InputBase, Switch, Toolbar, Typography } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import { Search } from '@material-ui/icons';
-import React, { Component } from 'react';
-import { withLanguageContext } from './contexts/LanguageContext';
+import React, { useContext } from 'react';
+import { LanguageContext } from './contexts/LanguageContext';
 import { ThemeContext } from './contexts/ThemeContext';
 import styles from './styles/NavBarStyles';
 
@@ -24,41 +24,38 @@ const text = {
   },
 };
 
-class NavBar extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { classes } = this.props;
-    const { isDark, toggleTheme } = this.context;
-    const { language } = this.props.languageContext;
-    return (
-      <div className={classes.root}>
-        <AppBar position="static" color={isDark ? 'default' : 'primary'}>
-          <Toolbar>
-            <IconButton className={classes.menuButton} color="inherit">
-              <span>{text[language].emoji}</span>
-            </IconButton>
-            <Typography className={classes.title} variant="h6" color="inherit">
-              {text[language].signInHere}
-            </Typography>
-            <Switch onChange={toggleTheme} />
-            <div className={classes.grow} />
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <Search />
-              </div>
-              <InputBase
-                placeholder={text[language].search}
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput,
-                }}
-              />
+function NavBar(props) {
+  const { classes } = props;
+  const { isDark, toggleTheme } = useContext(ThemeContext);
+  const { language } = useContext(LanguageContext);
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color={isDark ? 'default' : 'primary'}>
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit">
+            <span>{text[language].emoji}</span>
+          </IconButton>
+          <Typography className={classes.title} variant="h6" color="inherit">
+            {text[language].signInHere}
+          </Typography>
+          <Switch onChange={toggleTheme} />
+          <div className={classes.grow} />
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <Search />
             </div>
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
-  }
+            <InputBase
+              placeholder={text[language].search}
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
-export default withLanguageContext(withStyles(styles)(NavBar));
+export default withStyles(styles)(NavBar);
