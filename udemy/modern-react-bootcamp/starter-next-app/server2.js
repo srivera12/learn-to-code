f3586445b74b19d032fd67a016486aa040f8d5ea
Sarah Rivera <sarahkrivera@Sarahs-MBP.fios-router.home>
2021-12-15 15:12:32 -0500

    fetching, routing, and express with next js

---
+++ udemy/modern-react-bootcamp/starter-next-app/server2.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get('/post/:id', (req, res) => {
+    app.render(req, res, '/post', { id: req.params.id });
+  });
+
+  server.get('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(3000, (err) => {
+    if (err) throw err;
+    console.log('> Now serving on localhost:3000');
+  });
+});
