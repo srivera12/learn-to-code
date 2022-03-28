6abd47b73f695ea2d04eb13aa275975b3b0992db
srivera12 <sarahkrivera@gmail.com>
2022-03-28 15:47:43 -0400

    setting up web server and rendering pages in handlebars

--- udemy/complete-node/web-server/src/app.js
+++ udemy/complete-node/web-server/src/app.js
@@ -1,22 +1,66 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../public')));
+// paths
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
-// app.get('/help', (req, res) => {
-//   res.send({ name: 'Sarah', age: 27 });
-// });
+// set up handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
-// app.get('/about', (req, res) => {
-//   res.send('<h1>About Page</h1>');
-// });
+// setup static directory
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Sarah Rivera',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Page',
+    name: 'Sarah Rivera',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help Page',
+    name: 'Sarah Rivera',
+    message: 'This is where you go to get help.',
+  });
+});
 
 app.get('/weather', (req, res) => {
   res.send({
     location: `Showing weather for place and time.`,
     forecast: `Currently the weather is description. The temperature is temperature and the feels like is feelslike.`,
+  });
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: 'Help Page',
+    name: 'Sarah Rivera',
+    error: 404,
+    message: 'Help topic not found',
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: 'Error Page',
+    name: 'Sarah Rivera',
+    error: 404,
+    message: 'Page not found',
   });
 });
 
