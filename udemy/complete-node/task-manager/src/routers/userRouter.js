c5870e9622321b51dd8ecd3022eb924da17379c3
srivera12 <sarahkrivera@gmail.com>
2022-04-27 20:22:54 -0400

    added avatar image functionality

--- udemy/complete-node/task-manager/src/routers/userRouter.js
+++ udemy/complete-node/task-manager/src/routers/userRouter.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import User from '../models/user.js';
 import auth from '../middleware/auth.js';
+import multer from 'multer';
+import sharp from 'sharp';
 
 const userRouter = new express.Router();
 
@@ -80,6 +82,54 @@ userRouter.delete('/users/me', auth, async (req, res) => {
     res.send(req.user);
   } catch (err) {
     res.status(500).send(err);
+  }
+});
+
+// basic route for uploading avatar photo
+const upload = multer({
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please upload an image.'));
+    }
+    cb(undefined, true);
+  },
+});
+userRouter.post(
+  '/users/me/avatar',
+  auth,
+  upload.single('avatar'),
+  async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer();
+    req.user.avatar = buffer;
+    await req.user.save();
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
+// route to deleting avatar photo
+userRouter.delete('/users/me/avatar', auth, async (req, res) => {
+  req.user.avatar = undefined;
+  await req.user.save();
+  res.send();
+});
+
+// route to view user avatar by user id
+userRouter.get('/users/:id/avatar', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user || !user.avatar) {
+      throw new Error();
+    }
+    res.set('Content-Type', 'image/png');
+    res.send(user.avatar);
+  } catch (e) {
+    res.status(404).send();
   }
 });
 
